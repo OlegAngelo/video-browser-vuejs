41f9fc5c7e536d7f8c284c489a2ebbb7fdc9d516
balagtasangelo <ninoangelo.balagtas@sun-asterisk.com>
2021-07-07 17:55:04 +0800

    final commit

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <input @keyup.enter="onChange" />
+    </div> 
+</template>
+
+<script>
+export default {
+    name:'SearchBar',
+    methods:{
+        onChange(event) {
+            this.$emit('termChange', event.target.value);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    input{
+        width: 75%;
+    }
+
+    div{
+        text-align: center;
+        margin: 20px;
+    }
+</style>
